b3758452565d12205193305a3297425a16446796
leequanno1 <binquan1234@gmail.com>
2024-06-07 19:40:05 +0700

    check adding view

---
+++ app/src/main/java/com/example/taskmanagerment/TaskDetails.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagerment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
